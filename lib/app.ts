51366e10e617a1150f84cf18fcf02144099e2a90
Albert Vila <avilac4@gmail.com>
2020-08-19 00:50:05 +0200

    #2 Created the meeting model and implemented the mongoose module to connect ot the db.

--- lib/app.ts
+++ lib/app.ts
@@ -1,15 +1,18 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
 import { Routes } from "./routes/routes";
+import * as mongoose from "mongoose";
 
 class App {
 
     public app: express.Application = express();
     public router: Routes = new Routes();
+    public mongoUrl: string = 'mongodb://localhost/meeting_manager';
 
     constructor() {
         this.config();
-        this.router.routes(this.app);     
+        this.router.routes(this.app);
+        this.mongoSetup();
     }
 
     private config(): void{
@@ -17,6 +20,11 @@ class App {
         this.app.use(bodyParser.urlencoded({ extended: false }));
         // serving static files 
         this.app.use(express.static('public'));
+    }
+
+    private mongoSetup(): void{
+        mongoose.Promise = global.Promise;
+        mongoose.connect(this.mongoUrl);
     }
 
 }
